879933b5d0be39a5000883c5cb595f7b0df5c894
kozlov-oe <O.Kozlov@otpbank.ru>
2021-10-01 00:52:10 +0300

    complete clicker-counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.getElementById('count').innerText = `0`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'inc':
+        return state + 1;
+        case 'dec':
+            return state - 1;
+        case 'ref':
+            return state = 0;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+    return document.getElementById('count').innerText = `${store.getState()}`;
+});
+
+document.getElementById('countPlus').addEventListener('click', () => store.dispatch({type: 'inc'}));
+document.getElementById('countMinus').addEventListener('click', () => store.dispatch({type: 'dec'}));
+document.getElementById('countRefresh').addEventListener('click', () => store.dispatch({type: 'ref'}));
